46ff6b406175a9c5b9449e416314084c55f782bf
Priyanshi729 <priyanshisaini792@gmail.com>
2026-07-17 16:17:28 +0530

    made changes

--- models/assigned.go
+++ models/assigned.go
@@ -7,14 +7,14 @@ type AssignAssetRequest struct {
 }
 
 type AssetAssignmentHistory struct {
-	AssignmentID string    `db:"assignment_id" json:"assignment_id"`
-	AssetID      string    `db:"asset_id" json:"asset_id"`
-	Brand        string    `db:"brand" json:"brand"`
-	Model        string    `db:"model" json:"model"`
-	AssetType    string    `db:"asset_type" json:"asset_type"`
-	UserID       string    `db:"user_id" json:"user_id"`
-	Name         string    `db:"name" json:"name"`
-	Status       string    `db:"status" json:"status"`
-	AssignedOn   time.Time `db:"assigned_on" json:"assigned_on"`
-	ReturnedAt   time.Time `db:"returned_at" json:"returned_at"`
+	AssignmentID string     `db:"assignment_id" json:"assignment_id"`
+	AssetID      string     `db:"asset_id" json:"asset_id"`
+	Brand        string     `db:"brand" json:"brand"`
+	Model        string     `db:"model" json:"model"`
+	AssetType    string     `db:"asset_type" json:"asset_type"`
+	UserID       string     `db:"user_id" json:"user_id"`
+	Name         string     `db:"name" json:"name"`
+	Status       string     `db:"status" json:"status"`
+	AssignedOn   time.Time  `db:"assigned_on" json:"assigned_on"`
+	ReturnedAt   *time.Time `db:"returned_at" json:"returned_at"`
 }
